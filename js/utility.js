70ede579d062ad4f38967107ee59d482ad23f791
maruyamayuki2911 <yuki.maruyama2911@gmail.com>
2025-04-30 20:21:39 +0900

    202404302021

---
+++ js/utility.js
@@ -0,0 +1,39 @@
+import { config } from './config.js';
+
+// XXS対策
+export const sanitize = (str) => {
+  if (str) {
+    return str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;').replace(/'/g, '&#39;').replace(/"/g, '&quot;');
+  }
+}
+
+//メールアドレスのバリデーション
+export const emailValidation = (data) => {
+  if (!data.email || !data.email.trim()) {
+    return 'メールアドレスを入力してください。';
+  } else if (!/^[a-zA-Z0-9-._+/]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/.test(data.email)) {
+    return 'メールアドレスを正しく入力してください。';
+  }
+}
+
+// フォームデータを送信する処理
+export const sendFormData = async (data) => {
+  try {
+    const response = await fetch(`${config.apiUrl}/form`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTPステータス:${response.status}`);
+    }
+
+  } catch (error) {
+    console.log(error);
+    alert('しばらくたってからもう一度お試しください。');
+  }
+}
